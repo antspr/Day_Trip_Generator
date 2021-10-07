d79d7086e89ec56bdd7c6fc1341a23998c305fdb
antspr <antoinesprings@outlook.com>
2021-10-07 15:15:39 -0400

    Fixed integer/string conflict in the second input during debug

--- Day_04_Day_Trip_Generator/random_trip.py
+++ Day_04_Day_Trip_Generator/random_trip.py
@@ -5,6 +5,10 @@ restaurants = ["Morton's","Nobu","McCormick's","a Speakeasy"," a Dive Bar"," a P
 vehicles = ["Car","Bus","Train","Bicycle","Walking","Taxi","Horse and Buggy","Electric Scooter"]
 activities = ["Concert","Play","Niteclub","Bowling","Sightseeing","Museum","Voluteering","Networking"]
 approved = True #Want to use this to complete selection
+def randomization_tool(random_list):
+  for element in random_list:
+    element = (random.choice(random_list))
+    return element
 def choose_destination (list_of_places):
     approve_destination = True
     for place in list_of_places:
@@ -35,18 +39,19 @@ approval = input(f'We have put together a trip, {trip_package_mixed_string}, Wou
 if approval.upper == 'Y':
     print(f'The trip where {trip_package_mixed_string} selection process is complete. Congratulations!')
 elif approval.upper == 'n' or 'N':
-    new_selection = input("""What would you like to change? Choose "1" to change the destination, 
+    new_selection = int(input("""What would you like to change? 
+    Choose "1" to change the destination, 
     "2" for a different place to eat 
     "3" for a different mode of travel 
-    "4" for a different activity:""")
+    "4" for a different activity:"""))
     if new_selection == 1:
-      trip_package_list[0]= choose_destination(cities)
+      approved_package[0]= randomization_tool(cities)
     elif new_selection == 2:
-      trip_package_list[1]= choose_restaurant(restaurants)
+      approved_package[1]= randomization_tool(restaurants)
     elif new_selection == 3:
-      trip_package_list[2]= choose_vehicle(vehicles)
+      approved_package[2]= randomization_tool(vehicles)
     elif new_selection == 4:
-      trip_package_list[3]= choose_activity(activities)
+      approved_package[3]= randomization_tool(activities)
 print(f'Your updated trip now has you traveling to {approved_package[0]}, eating at {approved_package[1]}, traveling by {approved_package[2]}, and hanging out {approved_package[3]}')
 
 
