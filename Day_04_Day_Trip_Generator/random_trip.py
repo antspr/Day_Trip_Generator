07d409a1e660f21bfcea14e7cad5ff91d8c8446b
antspr <antoinesprings@outlook.com>
2021-10-08 10:04:58 -0400

    Updated

--- Day_04_Day_Trip_Generator/random_trip.py
+++ Day_04_Day_Trip_Generator/random_trip.py
@@ -4,7 +4,6 @@ cities = ['New York','Miami','Los Angeles','London','Paris','Dubai','Hong Kong',
 restaurants = ["Morton's","Nobu","McCormick's","a Speakeasy"," a Dive Bar"," a Picnic","McDonald's"," some Street Food"]
 vehicles = ["Car","Bus","Train","Bicycle","Walking","Taxi","Horse and Buggy","Electric Scooter"]
 activities = ["Concert","Play","Niteclub","Bowling","Sightseeing","Museum","Voluteering","Networking"]
-approved = True #Want to use this to complete selection
 def randomization_tool(random_list):
   for element in random_list:
     element = (random.choice(random_list))
@@ -32,26 +31,38 @@ def choose_activity (list_of_activities):
     for activity in list_of_activities:
         activity = (random.choice(list_of_activities))
         return(activity)
+approved = True #Want to use this to complete selection
 trip_package_list = [choose_destination(cities),choose_restaurant(restaurants),choose_vehicle(vehicles),choose_activity(activities)]
 trip_package_mixed_string = (f'You will be visiting: {trip_package_list[0]}, where you will be having {trip_package_list[1]} for dinner, traveling by way of {trip_package_list[2]}, and spend the day...{trip_package_list[3]}')
 approved_package = [trip_package_list[0],trip_package_list[1],trip_package_list[2],trip_package_list[3]]#would like to call "return" variable from function?
 approval = input(f'We have put together a trip, {trip_package_mixed_string}, Would you like to go on this trip? Y/N?')
-if approval.upper == 'Y':
+if approval.upper() == 'Y':
+    approved == False
     print(f'The trip where {trip_package_mixed_string} selection process is complete. Congratulations!')
-elif approval.upper == 'n' or 'N':
-    new_selection = int(input("""What would you like to change? 
-    Choose "1" to change the destination, 
-    "2" for a different place to eat 
-    "3" for a different mode of travel 
-    "4" for a different activity:"""))
-    if new_selection == 1:
-      approved_package[0]= randomization_tool(cities)
-    elif new_selection == 2:
-      approved_package[1]= randomization_tool(restaurants)
-    elif new_selection == 3:
-      approved_package[2]= randomization_tool(vehicles)
-    elif new_selection == 4:
-      approved_package[3]= randomization_tool(activities)
-print(f'Your updated trip now has you traveling to {approved_package[0]}, eating at {approved_package[1]}, traveling by {approved_package[2]}, and hanging out {approved_package[3]}')
-
+elif approval.upper() == 'N':
+      approved == False
+      new_selection = int(input("""What would you like to change? 
+      Choose "1" to change the destination, 
+      "2" for a different place to eat 
+      "3" for a different mode of travel 
+      "4" for a different activity:"""))
+      if new_selection == 1:
+          approved_package[0]= randomization_tool(cities)
+      elif new_selection == 2:
+          approved_package[1]= randomization_tool(restaurants)
+      elif new_selection == 3:
+          approved_package[2]= randomization_tool(vehicles)
+      elif new_selection == 4:
+          approved_package[3]= randomization_tool(activities)  
+      while approved== True: 
+          confirmed = input(f'''Your updated trip now has you traveling to {approved_package[0]},
+          eating at {approved_package[1]}, 
+          traveling by {approved_package[2]},
+          and hanging out {approved_package[3]} Please enter "Y" to confirm, "N" to ammend.''')
+          if confirmed.lower() == 'y':
+            approved = True
+            print("Your trip has been confirmed")
+          elif confirmed.lower() == 'n':
+            approved == False
+          print(new_selection) 
 
